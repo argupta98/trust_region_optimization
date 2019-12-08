679f742ab56303396107079c5a76b420ee517ab7
Arjun <argupta@mit.edu>
2019-12-08 14:09:23 -0500

    Trust optimization working.

--- trust_optimization.py
+++ trust_optimization.py
@@ -10,6 +10,13 @@ class TrustRegionTester(object):
     DATA_IDX = 0
     GT_IDX = 1
 
+    # Trust Region Problem parameters
+    ETA_1 = 0
+    ETA_2 = 0.25
+    ETA_3 = 0.75
+    M_1 = 0.25
+    M_2 = 2.0
+
     def __init__(self):
         # Setup models
         self.trust_model = self.make_linear_regression() 
@@ -26,7 +33,7 @@ class TrustRegionTester(object):
         # Generate Data
         self.train_data, self.val_data, self.test_data = self.generate_data()
 
-        self.epochs = 100
+        self.epochs = 100 
         self.lr = 0.0001
 
     def train(self):
@@ -51,19 +58,25 @@ class TrustRegionTester(object):
         x_values = torch.Tensor(np.random.randint(-100, 100, num_samples))
         y_values =  4 * x_values + 6
         x_values = x_values.unsqueeze(1)
-        y_values = y_values.unsqueeze(1)
+        y_values = y_values.unsqueeze(1) + torch.Tensor(np.random.normal(noise, 0, y_values.shape)).unsqueeze(1)
+        x_values = x_values + torch.Tensor(np.random.normal(noise, 0, x_values.shape))
         test_start = train_size + val_size
         return ((x_values[:train_size], y_values[:train_size]),
                (x_values[train_size:train_size + val_size], y_values[train_size:train_size + val_size]),
                (x_values[test_start: test_start + test_size], y_values[test_start: test_start + test_size]))
 
     def gd_fn_approximation(self, grad, gamma):
-        return (2 + gamma) * torch.mm(grad, grad)
+        d = self.d(grad, gamma)
+        return -(torch.mm(grad, d) + torch.mm(d, d))
+    
+    def d(self, grad, gamma):
+        return -float(1 / float(1 + gamma)) *  grad
 
     def train_trust(self):
         gamma = 1
         last_val_error = self.run_validation(self.trust_model)
         for epoch in range(self.epochs):
+            print("--- Iteration {} ---".format(epoch))
             predictions = self.trust_model(self.train_data[self.DATA_IDX])
             error = self.loss_fn(predictions, self.train_data[self.GT_IDX])
 
@@ -71,25 +84,37 @@ class TrustRegionTester(object):
             error.backward()
 
             # Temporarily Take a Step
-            prev_grad = torch.Tensor(self.trust_model.weight.grad)
-            self.trust_model.weight = torch.nn.Parameter(self.trust_model.weight - self.lr * (1 / float(1 + gamma)) * prev_grad)
+            prev_grad = self.trust_model.weight.grad.clone()
+            prev_weights = self.trust_model.weight.clone()
+            self.trust_model.weight = torch.nn.Parameter(self.trust_model.weight + self.d(prev_grad, gamma))
 
             val_error = self.run_validation(self.trust_model)
 
             self.writer.add_scalar("trust_region/val", val_error, epoch)
 
-            d_diff_err = 1
             c_numerator = (last_val_error - val_error)
+            self.writer.add_scalar("trust_region/val_diff", c_numerator, epoch)
 
             # Validation Error Increased, and will be stuck
             if c_numerator < 0:
                 print("Validation Increased!")
 
-            c =  c_numerator / float(self.gd_fn_approximation(prev_grad, gamma))
-            print("estimation: {} gamma: {}".format(c, gamma))
-            if c < 1:  # Step is too large, Abort.
-                gamma /= 2.0
-                self.trust_model.weight = torch.nn.Parameter(self.trust_model.weight + self.lr * (1 / float(1 + gamma)) * prev_grad)
+
+            approx = float(self.gd_fn_approximation(prev_grad, gamma))
+            c =  c_numerator / approx
+            print("validation_diff: {}  estimation: {} gamma: {} gdfn_approx: {} prev_grad: {}".format(c_numerator, c, gamma, approx, prev_grad))
+            if c <= self.ETA_1:  # Step is too large, Abort.
+                print("c too small, aborting...")
+                self.trust_model.weight = torch.nn.Parameter(prev_weights)
+            else:
+                last_val_error = val_error
+                
+
+            if c < self.ETA_2:
+                gamma /= self.M_1
+            elif self.ETA_3 < c:
+                gamma /= self.M_2 
+            self.writer.add_scalar("trust_region/gamma", gamma, epoch)
 
     def train_normal(self):
         top_validation = 10**10
